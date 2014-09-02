2ecd09dd755fb21f5ccb17d9ea2c0f5db03a8346
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2014-09-02 21:43:17 +0200

    Initial

---
+++ DOMTokenList.js
@@ -0,0 +1,89 @@
+;(function (window) {
+    /*if ('DOMTokenList' in window) {
+        return;
+    }*/
+
+    var inArray = function (array, value) {
+        var i;
+
+        for (i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                return i;
+            }
+        }
+
+        return -1;
+    };
+
+    var DOMTokenList2 = function () {
+        this.tokens = [];
+        this.length = 0;
+    };
+
+    DOMTokenList2.prototype.add = function () {
+        var i,
+            tokens = [].slice.call(arguments);
+
+        for (i = 0; i < tokens.length; i++) {
+            if (!this.contains(tokens[i])) {
+                this.tokens.push(tokens[i]);
+            }
+        }
+    };
+
+    DOMTokenList2.prototype.contains = function (token) {
+        if (token === '' || /\s/.test(token)) {
+            throw new Error('Token must not be empty or contain whitespace.');
+        }
+
+        return inArray(this.tokens, token) !== -1;
+    };
+
+    DOMTokenList2.prototype.item = function (index) {
+        return this.tokens[index] || null;
+    };
+
+    DOMTokenList2.prototype.remove = function () {
+        var i,
+            key,
+            tokens = [].slice.call(arguments);
+
+        for (i = 0; i < tokens.length; i++) {
+            key = inArray(this.tokens, tokens[i]);
+
+            if (key !== -1) {
+                this.tokens.splice(key, 1);
+            }
+        }
+    };
+
+    DOMTokenList2.prototype.toggle = function (token, force) {
+        if (token === '' || /\s/.test(token)) {
+            throw new Error('Token must not be empty or contain whitespace.');
+        }
+
+        if (this.contains(token)) {
+            if (!force) {
+                this.remove(token);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        if (force === false) {
+            return false;
+        }
+
+        this.add(token);
+
+        return true;
+    };
+
+    DOMTokenList2.prototype.toString = function () {
+        return this.tokens.join(' ');
+    };
+
+    window.DOMTokenList2 = DOMTokenList2;
+}(window));
---
+++ README.md
@@ -0,0 +1,9 @@
+# DOMTokenList polyfill
+
+A super strict polyfill for [DOMTokenList](http://dom.spec.whatwg.org/#interface-domtokenlist).
+
+## Todo
+* Tests
+* Support for classList
+* Support for relList
+* Implement everything in the spec.
