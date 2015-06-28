b8b2d7e6d734a816a19c29bff5e6b176f2b453ab
Bogdan Chadkin <trysound@yandex.ru>
2015-06-28 16:06:40 +0300

    Rearrange gulpfile Fix order

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,19 @@ var rename = require('gulp-rename');
 var size = require('gulp-size');
 var uglify = require('gulp-uglify');
 
-var files = './src/**/*.js';
+var files = ['src/DOMTokenList-newest.js', 'src/DOMTokenList.js', 'src/classList.js', 'src/relList.js'];
 
-gulp.task('concat', ['lint'], function () {
+gulp.task('build', function () {
     return gulp.src(files)
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
         .pipe(concat('domtokenlist.js'))
         .pipe(header('/*! DOMTokenlist shim | Copyright <%= new Date().getFullYear() %> Jonathan Wilsson and Bogdan Chadkin. */\n'))
         .pipe(size({
             showFiles: true
         }))
-        .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('lint', function () {
-    return gulp.src(files)
-        .pipe(jscs())
-        .on('error', function (err) {
-            console.log(err.message);
-        })
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
-
-gulp.task('uglify', ['concat'], function () {
-    return gulp.src('dist/domtokenlist.js')
+        .pipe(gulp.dest('dist'))
         .pipe(uglify({
             preserveComments: 'some'
         }))
@@ -40,11 +29,11 @@ gulp.task('uglify', ['concat'], function () {
         .pipe(size({
             showFiles: true
         }))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', ['default'], function () {
-    gulp.watch(files, ['default']);
+gulp.task('dev', ['build'], function () {
+    gulp.watch(files, ['build'])
 });
 
-gulp.task('default', ['uglify']);
+gulp.task('default', ['build']);
