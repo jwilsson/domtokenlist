40b2a1366b6e3a0bdc8859f0c15ea9f6c88b6821
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-12 20:56:57 +0200

    Pass SVG tests if SVG isn't supported

--- tests/tests-svg-classList.js
+++ tests/tests-svg-classList.js
@@ -1,8 +1,16 @@
 module('svg.classList');
 
 test('Add single class', function () {
-    var testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var testElement;
 
+    // Always pass if SVG isn't supported
+    if (typeof SVGElement === 'undefined') {
+        ok(true);
+
+        return;
+    }
+
+    testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     testElement.classList.add('class-1');
 
     var testClassName = typeof testElement.className === 'string' ? testElement.className : testElement.className.baseVal;
@@ -10,8 +18,16 @@ test('Add single class', function () {
 });
 
 test('Add multiple classes', function () {
-    var testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var testElement;
 
+    // Always pass if SVG isn't supported
+    if (typeof SVGElement === 'undefined') {
+        ok(true);
+
+        return;
+    }
+
+    testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     testElement.classList.add('class-1', 'class-2');
 
     var testClassName = typeof testElement.className === 'string' ? testElement.className : testElement.className.baseVal;
@@ -19,8 +35,16 @@ test('Add multiple classes', function () {
 });
 
 test('Remove single class', function () {
-    var testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var testElement;
 
+    // Always pass if SVG isn't supported
+    if (typeof SVGElement === 'undefined') {
+        ok(true);
+
+        return;
+    }
+
+    testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     testElement.classList.add('class-1', 'class-2');
     testElement.classList.remove('class-2');
 
@@ -29,8 +53,16 @@ test('Remove single class', function () {
 });
 
 test('Remove multiple classes', function () {
-    var testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var testElement;
 
+    // Always pass if SVG isn't supported
+    if (typeof SVGElement === 'undefined') {
+        ok(true);
+
+        return;
+    }
+
+    testElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     testElement.classList.add('class-1', 'class-2', 'class-3');
     testElement.classList.remove('class-2');
 
