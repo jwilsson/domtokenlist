310e9de6f86f2f442e3531e58acbd90d6a616f6a
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2016-04-13 11:40:03 +0200

    Revert abd3e01

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,39 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var filter = require('gulp-filter');
-var footer = require('gulp-footer');
 var header = require('gulp-header');
-var iife = require('gulp-iife');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var size = require('gulp-size');
 var uglify = require('gulp-uglify');
-var umd = require('gulp-umd');
 
-var copyright = '/*! DOMTokenlist shim | Copyright <%= new Date().getFullYear() %> Jonathan Wilsson and Bogdan Chadkin. */\n';
 var files = ['src/DOMTokenList-newest.js', 'src/DOMTokenList.js', 'src/classList.js', 'src/relList.js', 'src/svg.classList.js'];
 
-gulp.task('lint', function () {
+gulp.task('build', function () {
     return gulp.src(files)
         .pipe(jscs())
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
-});
-
-gulp.task('build', ['lint'], function () {
-    var f = filter('DOMTokenList.js', {
-        restore: true,
-    });
-
-    return gulp.src(files)
-        .pipe(f)
-        .pipe(footer('window.DOMTokenList = DOMTokenList;'))
-        .pipe(iife({
-            params: ['window'],
-            prependSemicolon: false,
-            useStrict: false,
-        }))
-        .pipe(header(";typeof window !== 'undefined' && "))
-        .pipe(f.restore)
         .pipe(concat('domtokenlist.js'))
-        .pipe(header(copyright))
+        .pipe(header('/*! DOMTokenlist shim | Copyright <%= new Date().getFullYear() %> Jonathan Wilsson and Bogdan Chadkin. */\n'))
         .pipe(size({
-            showFiles: true,
+            showFiles: true
         }))
         .pipe(gulp.dest('dist'))
         .pipe(uglify({
-            preserveComments: 'some',
+            preserveComments: 'some'
         }))
         .pipe(rename({
-            suffix: '.min',
+            suffix: '.min'
         }))
         .pipe(size({
-            showFiles: true,
+            showFiles: true
         }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-umd', ['lint'], function () {
-    return gulp.src('src/DOMTokenList.js')
-        .pipe(concat('domtokenlist-umd.js'))
-        .pipe(umd({
-            exports: function(file) {
-                return 'DOMTokenList';
-            },
-            namespace: function(file) {
-                return 'DOMTokenList';
-            },
-        }))
-        .pipe(header(copyright))
-        .pipe(gulp.dest('dist'))
-        .pipe(uglify({
-            preserveComments: 'some',
-        }))
-        .pipe(rename({
-            suffix: '.min',
-        }))
-        .pipe(gulp.dest('dist'));
+gulp.task('dev', ['build'], function () {
+    gulp.watch(files, ['build'])
 });
 
-gulp.task('dev', ['build', 'build-umd'], function () {
-    gulp.watch(files, ['build', 'build-umd'])
-});
-
-gulp.task('default', ['build', 'build-umd']);
+gulp.task('default', ['build']);
--- src/DOMTokenList.js
+++ src/DOMTokenList.js
@@ -1,125 +1,129 @@
-'use strict';
+;typeof window !== 'undefined' && (function (window) {
+    'use strict';
 
-var arr = [];
+    var arr = [];
 
-var inArray = function (array, value) {
-    var i;
-
-    if (arr.indexOf) {
-        return arr.indexOf.call(array, value);
-    }
-
-    for (i = 0; i < array.length; i++) {
-        if (array[i] === value) {
-            return i;
-        }
-    }
-
-    return -1;
-};
-
-var validateToken = function (token) {
-    var whitespace = /[\u0009\u000A\u000C\u000D\u0020]/;
-
-    if (token === '' || whitespace.test(token)) {
-        throw new Error('Token must not be empty or contain whitespace.');
-    }
-};
-
-var DOMTokenList = function (element, prop) {
-    var inst = this;
-    var i;
-    var values = [];
-
-    if (element && prop) {
-        inst.element = element;
-        inst.prop = prop;
-
-        if (element[prop]) {
-            values = element[prop].replace(/^\s+|\s+$/g, '').split(/\s+/);
-
-            for (i = 0; i < values.length; i++) {
-                inst[i] = values[i];
-            }
-        }
-    }
-
-    inst.length = values.length;
-};
-
-DOMTokenList.prototype = {
-    add: function () {
-        var inst = this;
-        var i;
-        var tokens = arguments;
-
-        for (i = 0; i < tokens.length; i++) {
-            validateToken(tokens[i]);
-
-            if (!inst.contains(tokens[i])) {
-                arr.push.call(inst, tokens[i]);
-            }
-        }
-
-        if (inst.element) {
-            inst.element[inst.prop] = inst;
-        }
-    },
-
-    contains: function (token) {
-        validateToken(token);
-
-        return inArray(this, token) !== -1;
-    },
-
-    item: function (index) {
-        return this[index] || null;
-    },
-
-    remove: function () {
-        var tokens = arguments;
-        var inst = this;
-        var key;
+    var inArray = function (array, value) {
         var i;
 
-        for (i = 0; i < tokens.length; i++) {
-            validateToken(tokens[i]);
+        if (arr.indexOf) {
+            return arr.indexOf.call(array, value);
+        }
 
-            key = inArray(inst, tokens[i]);
-
-            if (key !== -1) {
-                arr.splice.call(inst, key, 1);
+        for (i = 0; i < array.length; i++) {
+            if (array[i] === value) {
+                return i;
             }
         }
 
-        if (inst.element) {
-            inst.element[inst.prop] = inst;
+        return -1;
+    };
+
+    var validateToken = function (token) {
+        var whitespace = /[\u0009\u000A\u000C\u000D\u0020]/;
+
+        if (token === '' || whitespace.test(token)) {
+            throw new Error('Token must not be empty or contain whitespace.');
         }
-    },
+    };
 
-    toggle: function (token, force) {
+    var DOMTokenList = function (element, prop) {
         var inst = this;
+        var i;
+        var values = [];
 
-        if (inst.contains(token)) {
-            if (force) {
+        if (element && prop) {
+            inst.element = element;
+            inst.prop = prop;
+
+            if (element[prop]) {
+                values = element[prop].replace(/^\s+|\s+$/g, '').split(/\s+/);
+
+                for (i = 0; i < values.length; i++) {
+                    inst[i] = values[i];
+                }
+            }
+        }
+
+        inst.length = values.length;
+    };
+
+    DOMTokenList.prototype = {
+        add: function () {
+            var inst = this;
+            var i;
+            var tokens = arguments;
+
+            for (i = 0; i < tokens.length; i++) {
+                validateToken(tokens[i]);
+
+                if (!inst.contains(tokens[i])) {
+                    arr.push.call(inst, tokens[i]);
+                }
+            }
+
+            if (inst.element) {
+                inst.element[inst.prop] = inst;
+            }
+        },
+
+        contains: function (token) {
+            validateToken(token);
+
+            return inArray(this, token) !== -1;
+        },
+
+        item: function (index) {
+            return this[index] || null;
+        },
+
+        remove: function () {
+            var tokens = arguments;
+            var inst = this;
+            var key;
+            var i;
+
+            for (i = 0; i < tokens.length; i++) {
+                validateToken(tokens[i]);
+
+                key = inArray(inst, tokens[i]);
+
+                if (key !== -1) {
+                    arr.splice.call(inst, key, 1);
+                }
+            }
+
+            if (inst.element) {
+                inst.element[inst.prop] = inst;
+            }
+        },
+
+        toggle: function (token, force) {
+            var inst = this;
+
+            if (inst.contains(token)) {
+                if (force) {
+                    return true;
+                }
+
+                inst.remove(token);
+
+                return false;
+            } else {
+                if (force === false) {
+                    return false;
+                }
+
+                inst.add(token);
+
                 return true;
             }
+        },
 
-            inst.remove(token);
-
-            return false;
-        } else {
-            if (force === false) {
-                return false;
-            }
-
-            inst.add(token);
-
-            return true;
+        toString: function () {
+            return arr.join.call(this, ' ');
         }
-    },
+    };
 
-    toString: function () {
-        return arr.join.call(this, ' ');
-    }
-};
+    window.DOMTokenList = DOMTokenList;
+}(window));
